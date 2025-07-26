cd9c3510b39d95f185cb5ba31a5a1de4233c45e3
Aashrith Murari <aashrithmurari2004@gmail.com>
2025-07-26 19:35:58 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect('mongodb+srv://root:12345@tripsplit.tvh4wcu.mongodb.net/?retryWrites=true&w=majority&appName=Tripsplit');
     console.log("MongoDB connected");
   } catch (error) {
     console.error(error.message);
