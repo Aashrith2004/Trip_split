5727f8008b64f0675d0b2aba2630d3d3edf2506a
Aashrith Murari <aashrithmurari2004@gmail.com>
2025-07-26 19:52:34 +0530

    added server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,13 @@ const tripRoutes = require("./routes/tripRoutes");
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors(
+  {
+    origin: "https://trip-split-frontend.vercel.app",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"]
+  }
+));
 app.use(express.json());
 
 // Routes
