33117204b83ff3cdf67f6bbb2c3508d5ce97d0e1
Aashrith Murari <aashrithmurari2004@gmail.com>
2025-07-26 20:00:43 +0530

    added db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,30 @@
 const mongoose = require("mongoose");
+
+let cached = global.mongoose;
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
 const connectDB = async () => {
-  try {
-    await mongoose.connect('mongodb+srv://root:12345@tripsplit.tvh4wcu.mongodb.net/?retryWrites=true&w=majority&appName=Tripsplit');
-    console.log("MongoDB connected");
-  } catch (error) {
-    console.error(error.message);
-    process.exit(1);
+  if (cached.conn) {
+    return cached.conn;
   }
+
+  if (!cached.promise) {
+    const uri = process.env.MONGODB_URI;
+    if (!uri) {
+      throw new Error('Please define the MONGODB_URI environment variable');
+    }
+    cached.promise = mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }).then((mongoose) => {
+      console.log("MongoDB connected");
+      return mongoose;
+    });
+  }
+  cached.conn = await cached.promise;
+  return cached.conn;
 };
+
 module.exports = connectDB;
